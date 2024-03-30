4dd99e44868af7e37ff0f9101574c31a7070afdd
RafalPujer <rafal.pujer00@gmail.com>
2024-03-30 18:17:04 +0100

    added initial db migration

--- Tags/Context/TagsContext.cs
+++ Tags/Context/TagsContext.cs
@@ -5,10 +5,22 @@ namespace Tags.Context;
 
 public class TagsContext(DbContextOptions options) : DbContext(options)
 {
+    public const string CalcuateCountPercentFunctionName = "CalculateCountPercent";
+    public const string TagCountColumnName = "Count";
+    public const string TagTableName = "Tags";
+
     public DbSet<TagModel> Tags { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        const string CountPercentComputedSql = $"dbo.{CalcuateCountPercentFunctionName}(CAST(\"{TagCountColumnName}\" AS DECIMAL(38,18)))";
+
+        modelBuilder
+            .Entity<TagModel>()
+            .ToTable(TagTableName)
+            .Property(e => e.CountPercent)
+            .HasComputedColumnSql(CountPercentComputedSql);
+
         base.OnModelCreating(modelBuilder);
     }
 }
---
+++ Tags/Migrations/20240330172905_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tags.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false),
+                    CountPercent = table.Column<float>(type: "real", nullable: false, computedColumnSql: "dbo.CalculateCountPercent(CAST(\"Count\" AS DECIMAL(38,18)))")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
