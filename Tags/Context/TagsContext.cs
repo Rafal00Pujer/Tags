0afad67b634bf54f46cecdc50b582199eb7f95f2
RafalPujer <rafal.pujer00@gmail.com>
2024-03-30 18:03:53 +0100

    Add project files.

---
+++ Tags/Context/TagsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tags.Models;
+
+namespace Tags.Context;
+
+public class TagsContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<TagModel> Tags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Tags/Models/TagModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tags.Models;
+
+public class TagModel
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public int Count { get; set; }
+
+    public float CountPercent { get; set; }
+}
