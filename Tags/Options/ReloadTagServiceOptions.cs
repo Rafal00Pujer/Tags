0c29d362770b810da0bc54b52675dd222eccabf5
RafalPujer <rafal.pujer00@gmail.com>
2024-04-01 17:25:19 +0200

    added logging and options to reload tags feature

---
+++ Tags/Options/ReloadTagServiceOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tags.Options;
+
+public class ReloadTagServiceOptions
+{
+    public const string Name = "ReloadTagServiceOptions";
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int RequestedNumOnPages { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int RequestedPageSize { get; set; }
+
+    [Required]
+    public string RequestedSortType { get; set; } = null!;
+
+    public bool RequestedDescendingOrder { get; set; }
+
+    [Required]
+    [Range(0.01, 1.0)]
+    public double QuotaRemainingWarningThreshold { get; set; }
+
+    public bool ThrowOnBackOff { get; set; }
+}
--- Tags/Services/Implementations/ReloadTagsService.cs
+++ Tags/Services/Implementations/ReloadTagsService.cs
@@ -1,42 +1,65 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Net;
 using Tags.Context;
 using Tags.Models;
+using Tags.Options;
 using Tags.Services.Interfaces;
 
 namespace Tags.Services.Implementations;
 
 public class ReloadTagsService(
     TagsContext _context,
-    IStackExchangeApiService _stackExchangeApi)
+    IStackExchangeApiService _stackExchangeApi,
+    IOptions<ReloadTagServiceOptions> _options,
+    ILogger<ReloadTagsService> _logger)
     : IReloadTagsService
 {
     private readonly TagsContext _context = _context;
     private readonly IStackExchangeApiService _stackExchangeApi = _stackExchangeApi;
+    private readonly ReloadTagServiceOptions _options = _options.Value;
+    private readonly ILogger<ReloadTagsService> _logger = _logger;
 
     public async Task ReloadAsync()
     {
-        const int numOfPages = 10;
-        const int pageSize = 100;
-
         await using var transaction = _context.Database.BeginTransaction();
 
         await _context.Tags.ExecuteDeleteAsync();
         await _context.SaveChangesAsync();
 
-        for (var i = 1; i <= numOfPages; i++)
+        for (var i = 1; i <= _options.RequestedNumOnPages; i++)
         {
-            var response = await _stackExchangeApi.GetStackOverflowTags(i, pageSize);
+            var response =
+                await _stackExchangeApi
+                .GetStackOverflowTags(
+                    i,
+                    _options.RequestedPageSize,
+                    _options.RequestedSortType,
+                    _options.RequestedDescendingOrder);
 
             if (response.error_id.HasValue)
             {
                 transaction.Rollback();
-                throw new Exception();
+
+                throw new WebException("Stack Exchange Api request returned error." +
+                    $" Name: {response.error_name}," +
+                    $" Message: {response.error_message}");
             }
 
             if (response.backoff.HasValue)
             {
-                transaction.Rollback();
-                throw new Exception();
+                if (_options.ThrowOnBackOff)
+                {
+                    transaction.Rollback();
+
+                    throw new WebException($"Stack Exchange Api requested that we back off: {response.backoff.Value}");
+                }
+
+                _logger.LogWarning(
+                    "Stack Exchange Api requested that we back off: {time}",
+                    response.backoff.Value);
+
+                await Task.Delay(TimeSpan.FromSeconds(response.backoff.Value));
             }
 
             var tagsModels = response
@@ -51,10 +74,19 @@ public class ReloadTagsService(
             await _context.Tags.AddRangeAsync(tagsModels);
             await _context.SaveChangesAsync();
 
+            if ((double)response.quota_remaining / response.quota_max <= _options.QuotaRemainingWarningThreshold)
+            {
+                _logger.LogWarning(
+                    "We are approaching quota limit. remaining: {remaining}, max: {max}",
+                    response.quota_remaining,
+                    response.quota_max);
+            }
+
             if (!response.has_more)
             {
-                transaction.Rollback();
-                throw new Exception();
+                _logger.LogWarning("We requested more tags than Stack Exchange Api can provide.");
+
+                continue;
             }
         }
 
